280fe808a0801bbaf38c64bee9912319bfa99bd0
Andrew Osama <93442433+andrewosam@users.noreply.github.com>
2024-07-16 19:32:01 +0300

    Files
    Attendance-Task-For-Route-Teach-Summit

---
+++ Task-main/lib/injection/injection.config.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter_product_list/data/repositories/product_repository.dart'
+    as _i3;
+import 'package:flutter_product_list/presentation/cubit/product_cubit.dart'
+    as _i4;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+extension GetItInjectableX on _i1.GetIt {
+  _i1.GetIt init({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i3.ProductRepository>(() => _i3.ProductRepository());
+    gh.factory<_i4.ProductCubit>(
+        () => _i4.ProductCubit(gh<_i3.ProductRepository>()));
+    return this;
+  }
+}
